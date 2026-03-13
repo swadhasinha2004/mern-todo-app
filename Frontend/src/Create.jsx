39ab377585e40d7acf103aba88f7f6dd3b97aa61
swadhaCodes <swadhasinha2004@gmail.com>
2026-03-13 22:26:05 +0530

    Fix create component and refresh logic

--- Frontend/src/Create.jsx
+++ Frontend/src/Create.jsx
@@ -1,19 +1,33 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 
-function Create() {
-    const [task, setTask] = useState()
-    const handleAdd = () => {
-        axios.post('http://localhost:3001/add', {task: task})
-        .then(result => {
-          location.reload()
-        })
-        .catch(err => console.log(err))
-    }
+function Create({ refreshTodos }) {
+
+  const [task, setTask] = useState("")
+
+  const handleAdd = () => {
+    axios.post("https://mern-todo-app-aecd.onrender.com/add", { task })
+      .then(() => {
+        setTask("")        // clear input
+        refreshTodos()     // refresh todo list
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className='create_form'>
-      <input type="text" name="" id="" placeholder='Enter Task' onChange={(e) => setTask(e.target.value)}/>
-      <button type="button" onClick={handleAdd}>Add</button>
+
+      <input
+        type="text"
+        placeholder="Enter Task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button type="button" onClick={handleAdd}>
+        Add
+      </button>
+
     </div>
   )
 }
--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -4,58 +4,78 @@ import axios from 'axios'
 import { BsCircleFill, BsFillCheckCircleFill, BsFillTrashFill } from 'react-icons/bs'
 
 function Home() {
-    const [todos, setTodos] = useState([])
 
-    useEffect(() => {
-      axios.get('https://mern-todo-app-aecd.onrender.com/get')
+  const [todos, setTodos] = useState([])
+
+  // Function to fetch todos
+  const fetchTodos = () => {
+    axios.get("https://mern-todo-app-aecd.onrender.com/get")
       .then(result => setTodos(result.data))
       .catch(err => console.log(err))
-    }, [])
+  }
 
-   const handleEdit = (id) => {
-      axios.put('https://mern-todo-app-aecd.onrender.com/update/'+id)
-      .then(result => {
-        location.reload()
-      })
-      .catch(err => console.log(err))
-   }
+  // Load todos when page loads
+  useEffect(() => {
+    fetchTodos()
+  }, [])
 
-const handleDelete = (id) => {
-axios.delete('https://mern-todo-app-aecd.onrender.com/delete/'+id)
-      .then(result => {
-        location.reload()
-      })
+  // Mark task as done
+  const handleEdit = (id) => {
+    axios.put("https://mern-todo-app-aecd.onrender.com/update/" + id)
+      .then(() => fetchTodos())
       .catch(err => console.log(err))
-}
+  }
+
+  // Delete task
+  const handleDelete = (id) => {
+    axios.delete("https://mern-todo-app-aecd.onrender.com/delete/" + id)
+      .then(() => fetchTodos())
+      .catch(err => console.log(err))
+  }
 
   return (
     <div className='home'>
-        <h2>Todo List</h2>
-        <Create />
-        <br />
-        {
-            todos.length === 0
-            ?
-            <div><h2>No Record</h2></div>
-            :
-            todos.map(todo => (
-                <div className='task'>
-                  <div className="checkbox" onClick={() => handleEdit(todo._id)}>
-                    {todo.done ?
-                       <BsFillCheckCircleFill className='icon'></BsFillCheckCircleFill>
-                       : <BsCircleFill className='icon'/>
-                    }
-                   
-                <p className={todo.done ? "line_through" : ""}>{todo.task}</p> 
-                  </div>
-                   <div>     
-                    <span><BsFillTrashFill className='icon' 
-                    onClick={() => handleDelete(todo._id)}/></span>
-               </div>
-             </div>
+      <h2>Todo List</h2>
+
+      {/* Pass refresh function to Create component */}
+      <Create refreshTodos={fetchTodos} />
+
+      <br />
+
+      {
+        todos.length === 0
+          ?
+          <div><h2>No Record</h2></div>
+          :
+          todos.map(todo => (
+            <div className='task' key={todo._id}>
+
+              <div className="checkbox" onClick={() => handleEdit(todo._id)}>
+                {
+                  todo.done
+                    ? <BsFillCheckCircleFill className='icon' />
+                    : <BsCircleFill className='icon' />
+                }
+
+                <p className={todo.done ? "line_through" : ""}>
+                  {todo.task}
+                </p>
+              </div>
+
+              <div>
+                <span>
+                  <BsFillTrashFill
+                    className='icon'
+                    onClick={() => handleDelete(todo._id)}
+                  />
+                </span>
+              </div>
+
+            </div>
           ))
       }
-   </div>
+
+    </div>
   )
 }
 
